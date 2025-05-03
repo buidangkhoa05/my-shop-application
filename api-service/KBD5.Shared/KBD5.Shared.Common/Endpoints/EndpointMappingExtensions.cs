185f0c1f8f51f4c8b5f4e98dd7d7b28f511a4d91
khoabui <buidangkhoa05@gmail.com>
2025-05-04 01:47:09 +0700

    feat: Add endpoint mapping extensions and IEndpoint interface

---
+++ api-service/KBD5.Shared/KBD5.Shared.Common/Endpoints/EndpointMappingExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace KBD5.Shared.Common.Endpoints;
+
+public static class EndpointMappingExtensions
+{
+    public static IServiceCollection MapEndpoints(this IServiceCollection services, Assembly assembly)
+    {
+        var serviceDescriptors = assembly
+            .DefinedTypes
+            .Where(type => type is { IsAbstract: false, IsInterface: false }
+                           && type.IsAssignableTo(typeof(IEndpoint)))
+            .Select(type => ServiceDescriptor.Transient(typeof(IEndpoint), type))
+            .ToArray();
+
+        services.TryAddEnumerable(serviceDescriptors);
+
+        return services;
+    }
+
+    public static IApplicationBuilder MapEndpoints(
+        this WebApplication app,
+        RouteGroupBuilder? routeGroupBuilder = null)
+    {
+        var endpoints = app.Services.GetRequiredService<IEnumerable<IEndpoint>>();
+
+        IEndpointRouteBuilder builder = routeGroupBuilder is null ? app : routeGroupBuilder;
+
+        foreach (var endpoint in endpoints)
+        {
+            endpoint.MapEndpoint(builder);
+        }
+
+        return app;
+    }
+
+    public static RouteHandlerBuilder HasPermission(this RouteHandlerBuilder app, string permission)
+    {
+        return app.RequireAuthorization(permission);
+    }
+}
